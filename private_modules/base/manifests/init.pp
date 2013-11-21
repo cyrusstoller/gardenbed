7e6f3ba2577179aa2c47f2d39c8bd33100092b81
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-11-21 14:22:19 -0800

    Fixing a warning from the apt module

--- private_modules/base/manifests/init.pp
+++ private_modules/base/manifests/init.pp
@@ -17,4 +17,16 @@ class base (
     deployer_username       => $deployer_username,
     deployer_public_ssh_key => $deployer_public_ssh_key,
   }
+  
+  # from http://forge.puppetlabs.com/puppetlabs/apt
+  class { 'apt':
+    # always_apt_update    => false,
+    # disable_keys         => undef,
+    # proxy_host           => false,
+    # proxy_port           => '8080',
+    # purge_sources_list   => false,
+    # purge_sources_list_d => false,
+    # purge_preferences_d  => false,
+    update_timeout       => undef
+  }
 }
