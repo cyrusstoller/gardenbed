f7f3c9dd0f419a7a239e6a62cfba07b6bc9f6672
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-11-25 00:04:31 -0800

    Commenting the upgrade_debian_based_puppet.sh

--- scripts/upgrade_debian_based_puppet.sh
+++ scripts/upgrade_debian_based_puppet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# based on http://docs.puppetlabs.com/guides/installation.html#installing-puppet-1
+
 apt-get install --yes lsb-release
 DISTRIB_CODENAME=$(lsb_release --codename --short)
 DEB="puppetlabs-release-${DISTRIB_CODENAME}.deb"
@@ -15,3 +17,6 @@ fi
 
 sudo apt-get update
 sudo apt-get install --yes puppet
+
+# # to specify a specific version use http://www.debian.org/doc/manuals/apt-howto/ch-apt-get.en.html
+# sudo apt-get install --yes puppet=3.3.2
