f04959c4d1235ce9826d4028391af2aae8ed47d1
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-11-26 00:22:14 -0800

    Refactoring the encoding and locale variables when setting up postgresql

--- private_modules/base_db/manifests/postgresql.pp
+++ private_modules/base_db/manifests/postgresql.pp
@@ -3,12 +3,14 @@ class base_db::postgresql (
   $postgres_roles     = {},
   $postgres_databases = {},
   $postgres_version   = '9.3',
+  $postgres_encoding  = 'UTF8',
+  $postgres_locale    = 'en_US.UTF-8',
 ) {
   class { 'postgresql::globals':
     manage_package_repo => true,
     version             => $postgres_version,
-    encoding            => 'UTF8',
-    locale              => 'en_US.UTF-8'
+    encoding            => $postgres_encoding,
+    locale              => $postgres_locale,
   }
   ->
   class { '::postgresql::server':
