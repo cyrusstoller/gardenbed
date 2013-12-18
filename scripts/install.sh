20eadcbf9178e6c57aef1571456ed5621e8bf785
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-12-18 11:51:34 -0800

    Ensuring that bundler is installed before provisioning the vm - fixed #7

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,7 +19,17 @@ fi
 
 echo "Starting install process ..."
 cd "$BASE_PATH"
-echo "Installing necessary ruby gems with bundler ..."
+
+which bundle
+if [ $? -eq 0 ]
+then
+  echo "bundler is already installed"
+else
+  echo "installing bundler ..."
+  gem install bundler
+fi
+
+echo "Installing necessary ruby gems using bundler ..."
 bundle install
 echo "Installing necessary puppet modules from the forge ..."
 bundle exec librarian-puppet install
