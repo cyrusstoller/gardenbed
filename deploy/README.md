61c000938f32261cd95f199a9022c68c1d4d7c0f
Cyrus Stoller <cyrus.stoller@gmail.com>
2014-04-02 14:04:37 -0700

    Added a script to handle only rsyncing the necessary files to a server - issue #23

--- deploy/README.md
+++ deploy/README.md
@@ -1,3 +1,3 @@
 - Execute the contents of `scripts/upgrade_debian_based_puppet.sh` to install the latest version of puppet.
-- rsync this directory with your server with `rsync -r . deployer@<<SERVER IP>>:/tmp/puppet`
+- rsync this manifests to your server with `deploy/rsync.sh deployer@<<SERVER IP>>:/tmp/puppet`
 - Execute the contents of `deploy/puppet_apply_with_args.sh` 
---
+++ deploy/rsync.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DESTINATION=$1
+TMP_DIRECTORY=`mktemp -d -t gardenbed_puppet`
+
+function invalid_usage {
+  echo
+  echo "Invalid usage of rsync.sh"
+  echo
+  echo "You need to provide a destination"
+  echo
+  echo "usage: $0 DESTINATION"
+  exit 1
+}
+
+function add_files_to_tmp_directory {
+	DIR=$( dirname "${BASH_SOURCE}" )
+	BASE_PATH="$DIR/.."
+
+	cp -r "$BASE_PATH/deploy" $TMP_DIRECTORY
+
+	cp -r "$BASE_PATH/hiera" $TMP_DIRECTORY
+	rm "${TMP_DIRECTORY}/hiera/common.yaml"* # Copy over common.yml files separately
+
+	cp -r "$BASE_PATH/manifests" $TMP_DIRECTORY
+	cp -r "$BASE_PATH/modules" $TMP_DIRECTORY
+	cp -r "$BASE_PATH/scripts" $TMP_DIRECTORY
+}
+
+function rsync_tmp_directory {
+	du -h -d 1 $TMP_DIRECTORY
+
+	echo "rsyncing puppet manifests to: ${DESTINATION}"
+	echo ">> rsync -r ${TMP_DIRECTORY} ${DESTINATION}"
+
+	rsync -r $TMP_DIRECTORY $DESTINATION
+
+	rm -rf $TMP_DIRECTORY
+}
+
+if [ -n "$1" ]
+then
+	add_files_to_tmp_directory
+	rsync_tmp_directory
+	echo "All done!"
+else
+	invalid_usage
+fi
