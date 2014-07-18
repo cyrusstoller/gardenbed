2dc36491517b8141803d3956e4a04d0797286e9c
Cyrus Stoller <cyrus.stoller@gmail.com>
2014-07-17 17:21:37 -0700

    Linting the manifests

--- private_modules/s3cmd/manifests/backup.pp
+++ private_modules/s3cmd/manifests/backup.pp
@@ -1,12 +1,15 @@
 # Setup the cron jobs to sync directories to s3
 define s3cmd::backup (
   $user,
-  $directory = $title,
-  $bucket
+  $bucket,
+  $directory = $title
 ) {
+  $destination = "s3://${bucket}/${::fqdn}/"
+  $options = '-r --no-encrypt --delete-removed'
+
   cron { "backing up ${directory}":
     ensure  => present,
-    command => "s3cmd sync -r --no-encrypt --delete-removed ${directory} s3://${bucket}/${fqdn}/",
+    command => "s3cmd sync ${options} ${directory} ${destination}",
     user    => $user,
     hour    => '4',
     minute  => '30',
