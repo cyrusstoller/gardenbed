d8eb90602851111b80e88ef25bf7d4eafd21f963
Cyrus Stoller <cyrus.stoller@gmail.com>
2016-09-30 00:34:04 -0700

    Updating the puppet installation script

--- scripts/upgrade_debian_based_puppet.sh
+++ scripts/upgrade_debian_based_puppet.sh
@@ -4,13 +4,13 @@
 
 apt-get install --yes lsb-release
 DISTRIB_CODENAME=$(lsb_release --codename --short)
-DEB="puppetlabs-release-${DISTRIB_CODENAME}.deb"
+DEB="puppetlabs-release-pc1-${DISTRIB_CODENAME}.deb"
 DEB_PROVIDES="/etc/apt/sources.list.d/puppetlabs.list" # Assume that this file's existence means we have the Puppet Labs repo added
 
 if [ ! -e $DEB_PROVIDES ]
 then
   # Print statement useful for debugging, but automated runs of this will interpret any output as an error
-  print "Could not find $DEB_PROVIDES - fetching and installing $DEB"
+  echo "Could not find $DEB_PROVIDES - fetching and installing $DEB"
   wget -q http://apt.puppetlabs.com/$DEB
   sudo dpkg -i $DEB
 fi
